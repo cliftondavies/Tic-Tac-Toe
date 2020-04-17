269fda2ef47950de28f441b0960dee701778ca2e
cliftondavies <clifton.davies@outlook.com>
2020-04-17 20:57:36 +0100

    Add Game Logic

---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+# rubocop:disable Style/ClassVars
+class Player
+  attr_accessor :name
+  attr_reader :values
+
+  @@names = []
+  @@plays = []
+
+  def self.list_names
+    @@names
+  end
+
+  def self.list_plays
+    @@plays
+  end
+
+  def self.reset
+    @@names = []
+    @@plays = []
+  end
+
+  def initialize(name)
+    @name = name
+    @values = []
+    @@names << @name
+    @@plays << @values
+  end
+
+  def play(value)
+    values << value
+  end
+end
---
+++ lib/tictactoe.rb
@@ -0,0 +1,16 @@
+class TicTacToe
+  WIN = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+         [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+
+  attr_reader :outcome
+
+  def initialize
+    @outcome = 'draw'
+  end
+
+  def check(player_no)
+    @outcome = 'win'
+    message = "***Congrats, #{Player.list_names[player_no]}. You WON!!!***\n"
+    message
+  end
+end
