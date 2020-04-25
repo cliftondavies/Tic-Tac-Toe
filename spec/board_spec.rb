4157731ffc273f67f1de63edf4730dd56ff36012
cliftondavies <clifton.davies@outlook.com>
2020-04-25 23:34:59 +0100

    Refactor specs for Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,20 +1,26 @@
 require './lib/board.rb'
 
 describe Board do
+  subject { Board.new }
+
   describe '#update' do
+    let(:position) { 1 }
+
     context 'when player number is even' do
+      let(:player_no) { 0 }
+
       it 'marks X at the position specified by value' do
-        board = Board.new
-        board.update(1, 0)
-        expect(board.position[1]).to eql('X')
+        subject.update(position, player_no)
+        expect(subject.position[1]).to eql('X')
       end
     end
 
     context 'when player number is odd' do
+      let(:player_no) { 1 }
+
       it 'marks O at the position specified by value' do
-        board = Board.new
-        board.update(1, 1)
-        expect(board.position[1]).to eql('O')
+        subject.update(position, player_no)
+        expect(subject.position[1]).to eql('O')
       end
     end
   end
