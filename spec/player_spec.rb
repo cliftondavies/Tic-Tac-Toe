37caf298b48a1063232e5005833bc106f16026db
cliftondavies <clifton.davies@outlook.com>
2020-04-25 23:48:09 +0100

    Refactor specs for Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,17 +1,24 @@
 require './lib/player.rb'
+# rubocop:disable Style/ClassVars
 
 describe Player do
   describe '.list_names' do
+    let(:player1_name) { 'Clifton' }
+    let(:player2_name) { 'Terezie' }
+
     it 'returns the names of both players' do
-      @@names = %w[Clifton Terezie]
-      expect(Player.list_names).to eql(%w[Clifton Terezie])
+      @@names = [player1_name, player2_name]
+      expect(Player.list_names).to eql(@@names)
     end
   end
 
   describe '.list_plays' do
+    let(:player1_plays) { [1, 2, 3] }
+    let(:player2_plays) { [4, 5, 9] }
+
     it 'returns the moves of both players' do
-      @@plays = [[1, 2, 3], [4, 5, 9]]
-      expect(Player.list_plays).to eql([[1, 2, 3], [4, 5, 9]])
+      @@plays = [player1_plays, player2_plays]
+      expect(Player.list_plays).to eql(@@plays)
     end
   end
 
@@ -23,10 +30,15 @@ describe Player do
   end
 
   describe '#play' do
+    let(:name) { 'Clifton' }
+    let(:value) { 2 }
+
     it 'stores the moves made by a particular player' do
-      player = Player.new('Clifton')
-      player.play(2)
-      expect(player.values).to eql([2])
+      player = Player.new(name)
+      player.play(value)
+      expect(player.values).to include(value)
     end
   end
 end
+
+# rubocop:enable Style/ClassVars
